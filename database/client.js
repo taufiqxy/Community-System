454a9f5edfafd18c913f51de98f57f502324f1f5
taufiqx <taufiq.xy@gmail.com>
2023-01-28 20:24:55 +0700

    SET variable .env File

--- database/client.js
+++ database/client.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const { Client } = require('pg');
 
 const client = new Client({
-    user: 'developer',
-    host: 'localhost',
-    database: 'companydb',
-    password: 'your_password',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
 module.exports = { client };
