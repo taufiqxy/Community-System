819da6bd5aa4805a60071516e690a71574e1a230
taufiqx <taufiq.xy@gmail.com>
2023-02-03 08:40:34 +0700

    Add FlashMsg in Loggin

--- controllers/landingController/halamanLoginController.js
+++ controllers/landingController/halamanLoginController.js
@@ -2,7 +2,8 @@ const { client } = require('../../database/client');
 const { pool } = require('../../database/pool');
 
 const halamanLoginController = async (request, h) => {
-    return h.view('landingArea/login.njk');
+    const flashMsg = request.yar.flash('Failed Loggin'); // get flash message
+    return h.view('landingArea/login.njk', { flashMsg });
 };
 
 module.exports = { halamanLoginController };
--- controllers/landingController/loginController.js
+++ controllers/landingController/loginController.js
@@ -7,6 +7,7 @@ const loginController = async (request, reply) => {
     const account = internals.users.find((user) => user.email === email);
 
     if (!account || !(await Bcrypt.compare(password, account.password))) {
+        request.yar.flash('Failed Loggin', 'Login Gagal, Pastikan Email dan password Anda Benar!'); // set flash message
         return reply.redirect(urls.pageLogin);
     }
 
