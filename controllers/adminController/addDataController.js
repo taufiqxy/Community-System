5d4b638c7fd0e154062b9baf8a7c8f2a09cfc70f
taufiqx <taufiq.xy@gmail.com>
2023-02-04 14:25:04 +0700

    Set 2 Condition Flash Message

--- controllers/adminController/addDataController.js
+++ controllers/adminController/addDataController.js
@@ -9,10 +9,17 @@ const addDataController = async (request, h) => {
     try {
         const result = await pool.query(`insert into karyawan values
                                         ('${id}', '${namaLengkap}', '${email}', '${alamat}')`);
-        request.yar.flash('success add', 'Data Sukses Ditambahkan!'); // set flash message
+        // set success flash message
+        request.yar.flash('flashMsg add', {
+            status: 'success', msg: 'Data Sukses Ditambahkan!',
+        });
         return h.redirect('/admin/add-data-page');
     } catch (err) {
-        return 'Gagal Menambahkan Data';
+        // set failed flash message
+        request.yar.flash('flashMsg add', {
+            status: 'failed', msg: 'Data Gagal Ditambah. Periksa Kembali Data Anda!',
+        });
+        return h.redirect('/admin/add-data-page');
     }
 };
 
--- controllers/adminController/pageAddController.js
+++ controllers/adminController/pageAddController.js
@@ -2,7 +2,7 @@ const { client } = require('../../database/client');
 const { pool } = require('../../database/pool');
 
 const pageAddController = (request, h) => {
-    const flashMsg = request.yar.flash('success add'); // get flash message
+    const flashMsg = request.yar.flash('flashMsg add'); // get flash message
     return h.view('adminArea/add.njk', { flashMsg });
 };
 
