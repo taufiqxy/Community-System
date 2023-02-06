1ccb2f7c0041209e39a0764f0127322acd8c8383
taufiqx <taufiq.xy@gmail.com>
2023-02-06 20:58:59 +0700

    Login Admin from Database

--- controllers/landingController/loginController.js
+++ controllers/landingController/loginController.js
@@ -1,16 +1,32 @@
 const Bcrypt = require('bcrypt');
-const { internals } = require('../../dumbData');
+const { pool } = require('../../database/pool');
 const { urls } = require('../../routes/urls');
 
 const loginController = async (request, reply) => {
+    // get data from client
     const { role, email, password } = request.payload;
-    const account = internals.users.find((user) => user.email === email);
 
-    if (!account || !(await Bcrypt.compare(password, account.password))) {
+    // Non-admins are barred from logging in for a limited time.
+    if (!(role === 'admin')) {
+        return 'Software is under maintenance!';
+    }
+
+    // get role and password from db
+    let result = '';
+    try {
+        result = await pool.query('SELECT * FROM admin');
+        result = result.rows; // get only row of data
+        [result] = result; // destructuring result without array []
+    } catch (e) {
+        return 'Data failed to be fetched from the database.';
+    }
+
+    // check and compare the data sent by the client
+    if (!(email === result.email) || !(await Bcrypt.compare(password, result.password)) || result === '') {
         // set information failed login via flash message
         request.yar.flash(
             'Failed Loggin',
-            'Login Gagal. Pastikan Memasukan Email dan password Anda dengan Benar!',
+            'Login Gagal. Pastikan Anda Memasukan Email dan Password dengan Benar!',
             );
 
         // send old value via flash message to repopulate form
@@ -19,7 +35,7 @@ const loginController = async (request, reply) => {
     }
 
     // set cookie
-    request.cookieAuth.set({ id: account.id });
+    request.cookieAuth.set({ id: result.id });
     return reply.redirect(urls.pageShow);
 };
 
