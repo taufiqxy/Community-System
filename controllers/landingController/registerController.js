b83497248a07b5dd845ff03693f158005a6de749
taufiqx <taufiq.xy@gmail.com>
2023-02-23 15:33:40 +0700

    Add Code Activation and Remove photoProfile

--- controllers/landingController/registerController.js
+++ controllers/landingController/registerController.js
@@ -2,6 +2,8 @@ const baseDirectory = process.cwd();
 const Joi = require('joi');
 const fs = require('fs');
 
+const filetype = import('file-type');
+
 // schema validation with Joi
 const schema = Joi.object({
     email: Joi.string(),
@@ -12,12 +14,10 @@ const schema = Joi.object({
     repassword: Joi.ref('password'),
 });
 
-// file validation
-
 const registerController = async (request, h) => {
     // Get Requested Data
     const {
-        email, name, address, birthDate, password, repassword,
+        email, name, address, birthDate, photoProfile, password, repassword,
     } = await request.payload;
 
     // validate data with joi
