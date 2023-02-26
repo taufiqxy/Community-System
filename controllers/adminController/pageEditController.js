d5a5e5d513ff6e10ec99076b777c5aeabb04b451
taufiqx <taufiq.xy@gmail.com>
2023-02-26 09:29:36 +0700

    Fix Update Process In Admin

--- controllers/adminController/pageEditController.js
+++ controllers/adminController/pageEditController.js
@@ -5,12 +5,19 @@ const pageEditController = async (request, h) => {
     const { id } = request.params;
     try {
         const result = await pool.query(`SELECT * FROM karyawan where id='${id}'`);
-        const aKaryawan = result.rows;
-        const listKaryawan = aKaryawan.map((x) => {
-            return [x.id, x.name, x.email, x.address, x.birth_date, x.password];
-        });
-        // return listKaryawan;
-        return h.view('adminArea/edit.njk', { id, aKaryawan });
+        let aKaryawan = result.rows;
+        [aKaryawan] = aKaryawan;
+        // const birthDate = {
+        //     year: aKaryawan.birth_date.getFullYear(),
+        //     month: aKaryawan.birth_date.getMonth() + 1,
+        //     day: aKaryawan.birth_date.getDate(),
+        // };
+        const birthDate = aKaryawan.birth_date.getFullYear().toString()
+                        + '-0'.toString()
+                        + (aKaryawan.birth_date.getMonth() + 1).toString()
+                        + '-0'.toString()
+                        + aKaryawan.birth_date.getDate().toString();
+        return h.view('adminArea/edit.njk', { id, aKaryawan, birthDate });
     } catch (err) {
         return err;
     }
